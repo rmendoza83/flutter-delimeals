a27ca95b3ef617f38d0308d263914bf54e9d2868
rmendoza83 <46385631+rmendoza83@users.noreply.github.com>
2020-04-12 20:02:59 -0500

    Category Navigation
    Adding Category Navigation

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,3 +1,4 @@
+import 'package:delimeals/category_meals_page.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -6,24 +7,41 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsPage();
+        }
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+    return InkWell(
+      onTap: () { 
+        selectCategory(context);
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
